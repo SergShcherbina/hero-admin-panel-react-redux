939916e17daacd7d5d0133f4071223355c301f63
Serg <1990sergik27@gmail.com>
2022-10-19 13:09:21 +0300

    add_middleware_&_ReduxToolKit-configureStore()-204

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
-import { createStore, combineReducers, applyMiddleware, compose} from 'redux';
-import ReduxThunk from 'redux-thunk'                                //библиотека которая позволяет диспетчить не объекты а функции
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+import { configureStore } from '@reduxjs/toolkit';
 
-const store = createStore ( 
-                        combineReducers({heroes, filters}),                   //комбинируем redusers
-                        compose(applyMiddleware(ReduxThunk),                  //комбинируем ф-и (Middleware и REDUX_DEVTOOLS)     
-                            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-)
+const stringMiddleware = () => (next) => (action) => {
+    if(typeof action === 'string') {
+        return next ({
+            type: action
+        })    
+    }
+    return next(action)
+}
+// const store = createStore ( 
+//                         combineReducers({heroes, filters}),                   //комбинируем redusers
+//                         compose(applyMiddleware(ReduxThunk),                  //комбинируем ф-и (Middleware и REDUX_DEVTOOLS)     
+//                             window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// )
+
+const store = configureStore({
+    reduser: {heroes, filters},
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(stringMiddleware)
+})
 
 export default store;
